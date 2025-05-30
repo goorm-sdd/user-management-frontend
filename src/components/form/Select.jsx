156b12b6c49f652db08294b04ca31c538d5018ad
sarajeong17 <sarajeong17@gmail.com>
2025-05-30 16:24:17 +0900

    feat: ts -> js 로 수정

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
+import { useState } from "react"
 
 const Select = ({
   options,
   placeholder = "Select an option",
   onChange,
   className = "",
-  defaultValue = "",
+  defaultValue = ""
 }) => {
   // Manage the selected value
-  const [selectedValue, setSelectedValue] = useState<string>(defaultValue);
+  const [selectedValue, setSelectedValue] = useState(defaultValue)
 
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setSelectedValue(value);
-    onChange(value); // Trigger parent handler
-  };
+  const handleChange = e => {
+    const value = e.target.value
+    setSelectedValue(value)
+    onChange(value) // Trigger parent handler
+  }
 
   return (
     <select
@@ -35,7 +35,7 @@ const Select = ({
         {placeholder}
       </option>
       {/* Map over options */}
-      {options.map((option) => (
+      {options.map(option => (
         <option
           key={option.value}
           value={option.value}
@@ -45,7 +45,7 @@ const Select = ({
         </option>
       ))}
     </select>
-  );
-};
+  )
+}
 
-export default Select;
+export default Select
