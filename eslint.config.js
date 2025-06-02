c7c48e42ddfaff161f701d48021665266c9b86c2
sarajeong17 <sarajeong17@gmail.com>
2025-06-02 21:26:58 +0900

    refactor: biome lint 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
+  js.configs.recommended,
   {
     ignores: ['dist', 'node_modules'],
   },
   {
-    files: ['**/*.js', '**/*.jsx'],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // ✅ JSX 파싱 허용
+        },
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    settings: {
+      react: {
+        version: 'detect', // 자동 React 버전 감지
+      },
+    },
     rules: {
-      ...js.configs.recommended.rules,
+      'no-unused-vars': 'off',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-]
+];
