ad2cdce030e86f645c7e44804d39212c972b6ddc
sarajeong17 <sarajeong17@gmail.com>
2025-06-02 20:43:24 +0900

    refactor: 안쓰는 라이브러리 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import "swiper/swiper-bundle.css";
-import "flatpickr/dist/flatpickr.css";
-import App from "./App.jsx";
 import { AppWrapper } from "./components/common/PageMeta.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import App from "./App.jsx";
+import "./index.css";
+import "flatpickr/dist/flatpickr.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
