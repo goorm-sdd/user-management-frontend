1ce875fd03235e1bf8c2fa63bb17f37cc26fcf34
ssoogit <ssoogit02@gmail.com>
2025-06-03 14:44:44 +0900

    feat: 로그인 페이지 템플릿을 프로젝트에 맞게 수정

--- src/components/auth/SignInForm.jsx
+++ src/components/auth/SignInForm.jsx
@@ -7,75 +7,101 @@ import Checkbox from "../form/input/Checkbox";
 import Button from "../ui/button/Button";
 
 const SignInForm = () => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
-  return (
-    <div className="flex flex-col flex-1">
-      <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
-        <div>
-          <div className="mb-5 sm:mb-8">
-            <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
-              Sign In
-            </h1>
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              Enter your email and password to sign in!
-            </p>
-          </div>
-          <div>
-            <form>
-              <div className="space-y-6">
+    const [showPassword, setShowPassword] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
+    return (
+        <div className="flex flex-col flex-1">
+            <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
                 <div>
-                  <Label>
-                    Email <span className="text-error-500">*</span>{" "}
-                  </Label>
-                  <Input placeholder="info@gmail.com" />
+                    <div className="mb-5 sm:mb-8">
+                        <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
+                            Sign In
+                        </h1>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">
+                            이메일과 비밀번호를 입력하고 로그인 하세요!
+                        </p>
+                    </div>
+                    <div>
+                        <form>
+                            <div className="space-y-6">
+                                <div>
+                                    <Label>
+                                        Email{" "}
+                                        <span className="text-error-500">
+                                            *
+                                        </span>{" "}
+                                    </Label>
+                                    <Input placeholder="info@gmail.com" />
+                                </div>
+                                <div>
+                                    <Label>
+                                        Password{" "}
+                                        <span className="text-error-500">
+                                            *
+                                        </span>{" "}
+                                    </Label>
+                                    <div className="relative">
+                                        <Input
+                                            type={
+                                                showPassword
+                                                    ? "text"
+                                                    : "password"
+                                            }
+                                            placeholder="Enter your password"
+                                        />
+                                        <span
+                                            onClick={() =>
+                                                setShowPassword(!showPassword)
+                                            }
+                                            className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2"
+                                        >
+                                            {showPassword ? (
+                                                <EyeIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
+                                            ) : (
+                                                <EyeCloseIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
+                                            )}
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <div className="flex items-center gap-3">
+                                        <Checkbox
+                                            checked={isChecked}
+                                            onChange={setIsChecked}
+                                        />
+                                        <span className="block font-normal text-gray-700 text-theme-sm dark:text-gray-400">
+                                            로그인 상태 유지
+                                        </span>
+                                    </div>
+                                    <Link
+                                        to="/reset-password"
+                                        className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
+                                    >
+                                        이메일 또는 비밀번호를 잊어버렸나요?
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Button className="w-full" size="sm">
+                                        Sign in
+                                    </Button>
+                                </div>
+                                <div className="flex items-center gap-2">
+                                    <span className="block font-normal text-gray-700 text-theme-sm dark:text-gray-400">
+                                        회원이 아니신가요?
+                                    </span>
+                                    <Link
+                                        to="#"
+                                        className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
+                                    >
+                                        회원가입
+                                    </Link>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div>
-                  <Label>
-                    Password <span className="text-error-500">*</span>{" "}
-                  </Label>
-                  <div className="relative">
-                    <Input
-                      type={showPassword ? "text" : "password"}
-                      placeholder="Enter your password"
-                    />
-                    <span
-                      onClick={() => setShowPassword(!showPassword)}
-                      className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2"
-                    >
-                      {showPassword ? (
-                        <EyeIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
-                      ) : (
-                        <EyeCloseIcon className="fill-gray-500 dark:fill-gray-400 size-5" />
-                      )}
-                    </span>
-                  </div>
-                </div>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center gap-3">
-                    <Checkbox checked={isChecked} onChange={setIsChecked} />
-                    <span className="block font-normal text-gray-700 text-theme-sm dark:text-gray-400">
-                      Keep me logged in
-                    </span>
-                  </div>
-                  <Link
-                    to="/reset-password"
-                    className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
-                  >
-                    Forgot password?
-                  </Link>
-                </div>
-                <div>
-                  <Button className="w-full" size="sm">
-                    Sign in
-                  </Button>
-                </div>
-              </div>
-            </form>
-          </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
 export default SignInForm;
